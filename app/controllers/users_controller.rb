9f16037a8dc4a77c19099d11aeb6cd478cb44b8f
YuriFaraf <yura96@gmail.com>
2020-06-12 01:46:16 +0500

    views rebuilding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Vadim',
+            username: 'installero',
+            avatar_url: 'https://goodprogrammer.ru/system/avatars/000/000/001/9a01a7b3165a3d0235e81bd678e47b5ae5224770_x300.jpg'
+        ),
+        User.new(
+            id: 2,
+            name: 'Misha',
+            username: 'aristofun'
+        )
+    ]
   end
 
   def new
@@ -14,5 +27,15 @@ class UsersController < ApplicationController
         username: 'installero',
         avatar_url: 'https://goodprogrammer.ru/system/avatars/000/000/001/9a01a7b3165a3d0235e81bd678e47b5ae5224770_x300.jpg'
     )
+
+    @questions = [
+        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+        Question.new(text: 'В чём смысл жизни?', created_at: Date.parse('27.03.2016'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
+
+  def sklonyator (quantity, form1, form2, form3)
+    if (11..14).include?(quantity % 100)
+      form3
+    elsif quantity % 10 == 1
+      form1
+    elsif (2..4).include?(quantity % 10)
+      form2
+    else
+      form3
+    end
+  end
 end
