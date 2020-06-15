01dd214435329b42b09fda6579cee2278c406aa5
YuriFaraf <yura96@gmail.com>
2020-06-15 15:38:57 +0500

    shows author of question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -40,8 +40,13 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    if current_user.present? && params[:question][:user_id].to_i == current_user.id
-      params.require(:question).permit(:user_id, :text, :answer)
+    if current_user.present?
+      params[:question][:author_id] = current_user.id
+      if params[:question][:user_id].to_i == current_user.id
+        params.require(:question).permit(:user_id, :text, :answer, :author_id)
+      else
+        params.require(:question).permit(:user_id, :text, :author_id)
+      end
     else
       params.require(:question).permit(:user_id, :text)
     end
