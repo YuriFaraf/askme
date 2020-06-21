042bc0e04051813871aa942d3b32b29103277152
YuriFaraf <yura96@gmail.com>
2020-06-21 21:07:22 +0500

    routes security fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
 
-  get 'sign_up' => 'users#new'
-  get 'delete' => 'users#destroy'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
