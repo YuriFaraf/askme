f91ff0fe54fe267ef5b2287fe3a323ba0aab1fde
YuriFaraf <yura96@gmail.com>
2020-08-14 17:11:56 +0500

    Hashtag.with_questions refactoring

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,12 +4,7 @@ class Hashtag < ApplicationRecord
   has_many :hashtag_questions, dependent: :destroy
   has_many :questions, through: :hashtag_questions
 
-  # scope :with_questions, -> do
-  #   left_outer_joins(:hashtag_questions).
-  #       where.not(hashtag_questions: {id: nil}).
-  #       distinct.order(:created_at)
-  # end
-  scope :with_questions, -> { where_not_exists(:hashtag_questions, id:nil ).distinct.order(:created_at) }
+  scope :with_questions, -> { where_exists(:hashtag_questions).order(:created_at) }
 
   validates :text, presence: true, uniqueness: true
 end
